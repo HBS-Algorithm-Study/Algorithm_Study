610d4885f06dceb0ee3ba5956251bc794846ec00
hyun331 <sus03319@gmail.com>
2024-05-27 17:05:31 +0900

    study_1

---
+++ 평균_1546.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();	//과목 수
+		int[] scores = new int[n];	//과목 점수
+		int max = 0;	//최고점
+		double sum = 0;
+		
+		for(int i=0; i<n; i++) {
+			scores[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(max < scores[i])
+				max = scores[i];
+		}
+		
+		for(int i=0; i<n; i++) {
+			sum += (double)scores[i]/max*100;
+		}
+		
+		
+		
+		
+		System.out.println(sum/n);
+	}
+
+}

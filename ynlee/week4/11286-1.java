d08bbe8c8e0264bf11c6c1d02b49109ec17aa614
KiWing <92567807+Ki-Wing@users.noreply.github.com>
2024-06-10 12:14:14 +0900

    feat: Update 11286-1.java

--- ynlee/week4/11286-1.java
+++ ynlee/week4/11286-1.java
@@ -1,37 +1,34 @@
+// 검색함
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                int abs1 = Math.abs(o1);
-                int abs2 = Math.abs(o2);
-                if (abs1 == abs2) {
-                    return o1 - o2; 
-                }
-                return abs1 - abs2; 
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+            if (absA == absB) {
+                return Integer.compare(a, b);
+            } else {
+                return Integer.compare(absA, absB);
             }
         });
         
         for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(br.readLine());
-            if (x == 0) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
                 if (pq.isEmpty()) {
-                    sb.append(0).append('\n');
+                    System.out.println(0);
                 } else {
-                    sb.append(pq.poll()).append('\n');
+                    System.out.println(pq.poll());
                 }
             } else {
-                pq.add(x);
+                pq.add(num);
             }
         }
-        
-        System.out.print(sb.toString());
     }
 }

c689d198baabd0ffd9b2e0c2846f290501e0b054
KiWing <92567807+Ki-Wing@users.noreply.github.com>
2024-06-10 16:25:37 +0900

    Update 12891.java

--- ynlee/week3/12891.java
+++ ynlee/week3/12891.java
@@ -10,14 +10,16 @@ public class Main {
         input = br.readLine().split(" ");
         
         int[] m_cnt = new int[4];
-        for (int i = 0; i < 4; i++) m_cnt[i] = Integer.parseInt(input[i]);
-        
+        for (int i = 0; i < 4; i++) {
+            m_cnt[i] = Integer.parseInt(input[i]);
+        }
         int[] c_cnt = new int[4];
         int v_cnt = 0;
         String bases = "ACGT";
         
-        for (int i = 0; i < P; i++) c_cnt[bases.indexOf(dna[i])]++;
-        
+        for (int i = 0; i < P; i++) {
+            c_cnt[bases.indexOf(dna[i])]++;
+        }
         if (c_cnt[0] >= m_cnt[0] && c_cnt[1] >= m_cnt[1] &&
             c_cnt[2] >= m_cnt[2] && c_cnt[3] >= m_cnt[3]) {
             v_cnt++;

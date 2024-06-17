bcd017f04bfd59f7c73a1bfcc07f4770cfc03cec
leem5514 <ikaros0569@gmail.com>
2024-06-17 16:33:43 +0900

    2164

--- leem/0610/2164.java
+++ leem/0610/2164.java
@@ -1,21 +1,5 @@
 import java.lang.*;
 import java.util.*;
-// public class Main { //시간초과
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int N = sc.nextInt();
-//         Stack<Integer> stack = new Stack<>();
-//         for (int i = N; i >= 1; i--) {
-//             stack.push(i);
-//         }
-//         while (stack.size() != 1) {
-//             stack.pop();
-//             int topCard = stack.pop();
-//             stack.add(0, topCard);
-//         }
-//         System.out.println(stack.peek());
-//     }
-// }
 // public class Main {// 인프런 코딩
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);

95e544076a4f2ab0bf8e9c2238be2b8b13a80b78
ki-wing <dldpsk151@gmail.com>
2024-06-26 10:30:27 +0900

    feat: Add 1929.java

---
+++ ynlee/week7/1929.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]); 
+        int M = Integer.parseInt(input[1]); 
+
+        for (int i = N; i <= M; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

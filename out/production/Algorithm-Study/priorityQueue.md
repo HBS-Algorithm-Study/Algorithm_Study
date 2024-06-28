6df1f2571c5aa9a4957fe565b0aede8c4aee5144
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-06-28 16:38:21 +0900

    java

---
+++ out/production/Algorithm-Study/priorityQueue.md
@@ -0,0 +1,3 @@
+### 우선순위 큐 & 삽입 정렬 정리
+
+[정리 내용](https://brindle-spot-76e.notion.site/5-6-17-f732c8886e5b437bab292576bcb6645c?pvs=4)
---
+++ src/Boj6443.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Boj6443 {
+    static List<List<String>> strings;
+    static boolean[] visited;
+    public static void main(String[] args) {
+        strings = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        // String[] strings = new String[N];
+        String[] temp = new String[N];
+        for (int i = 0; i < N; i++) {
+            strings.add(new ArrayList<>());
+            temp[i] = sc.next();
+            for (int j = 0; j < temp[i].length(); j++) {
+                strings.get(i).add(String.valueOf(temp[i].charAt(j)));
+            }
+            // System.out.println(strings.toString());
+        }
+
+    }
+    static String dfs(String[][] strings){
+
+
+
+        return ;
+    }
+}
